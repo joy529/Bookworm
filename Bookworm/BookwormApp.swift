e3fe7a2c7a830e954d17f0d10205919d908b5f9f
joy jain <joyshah204@gmial.com>
2024-02-08 13:23:29 +0530

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by JOY JAIN on 08/02/24.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
